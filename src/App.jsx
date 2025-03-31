c5bc5fbada2be0f0c925949d5a255823129d2ce0
Caleb Linduff <calinduff@gmail.com>
2025-03-31 15:41:02 -0400

    Switched to jolpica api for dropdowns

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,67 @@ import { useEffect, useState, useRef } from "react"
 import Table from "./Table";
 
 // const openF1Endpoint = "https://api.openf1.org/v1/";
-const openF1Endpoint = "http://127.0.0.1:8000/openf1/";
+// const openF1Endpoint = "http://127.0.0.1:8000/openf1/";
 // const jolpicaEndpoint = "http://api.jolpi.ca/";
-// const jolpicaEndpoint = "http://127.0.0.1:8000/jolpica/";
+const jolpicaEndpoint = "http://127.0.0.1:8000/jolpica/";
+
+const weekendEvents = [
+  {name: "FirstPractice", label: "Practice 1"},
+  {name: "SecondPractice", label: "Practice 2"},
+  {name: "ThirdPractice", label: "Practice 3"},
+  {name: "SprintQualifying", label: "Sprint Qualifying"},
+  {name: "SprintShootout", label: "Sprint Shootout"},
+  {name: "Qualifying", label: "Qualifying"},
+  {name: "Sprint", label: "Sprint"},
+  {name: "Race", label: "Race"},
+];
 
 
 export default function App() {
   const currYear = new Date().getFullYear();
 
   const [year, setYear] = useState(currYear);
-  const [meetingKey, setMeetingKey] = useState("");
-  const [meetingList, setMeetingList] = useState([]);
-  const [sessionKey, setSessionKey] = useState("");
-  const [sessionList, setSessionList] = useState([]);
+  const [round, setRound] = useState(0);
+  const [raceList, setRaceList] = useState([]);
+  const [event, setEvent] = useState(0);
+  const [eventList, setEventList] = useState([]);
 
-  const isMounted1 = useRef(false);
+  const isMounted = useRef(false);
+
+  function handleEventList() {
+    const allEvents = []
+    const race = raceList.find((race) => race.round == round)
+    for(let i = 0; i < weekendEvents.length; i++) {
+      if(race?.hasOwnProperty(weekendEvents[i].name)){
+        allEvents.push({name: weekendEvents[i].label, id: i+1, data: race[weekendEvents[i].name]})
+      }
+    }
+    allEvents.push({name: "Race", id: 8, data: {date: race?.date, time: race?.time}})
+    allEvents.sort((a,b) => new Date(a?.data.date + "T" + a?.data.time) - new Date(b?.data.date + "T" + b?.data.time))
+    console.log(allEvents)
+    return allEvents;
+  }
 
   useEffect(function() {
-    fetch(`${openF1Endpoint}meetings/year=${year}`)
+    fetch(`${jolpicaEndpoint}ergast/f1/${year}/races.json`)
     .then((res) => res.json())
     .then((data) => {
-      setMeetingKey(data[data.length - 1].meeting_key)
-      setMeetingList(data);
+      const raceData = data.MRData.RaceTable.Races
+      setRaceList(raceData);
+      setRound(raceData[0].round);
     })
   }, [year])
-  
 
   useEffect(function() {
-    if(isMounted1.current){
-      fetch(`${openF1Endpoint}sessions/meeting_key=${meetingKey}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setSessionKey(data[data.length - 1].session_key)
-        setSessionList(data);
-      })
-    } else{
-      isMounted1.current = true;
+    if(isMounted.current){
+      const allEvents = handleEventList();
+      setEventList(allEvents);
+      setEvent(allEvents[0]?.id)
+    } else {
+      isMounted.current = true;
     }
-  }, [meetingKey])
+    
+  }, [round])
 
   return (
     <>
@@ -49,19 +72,18 @@ export default function App() {
         <option value={currYear - 1}>{currYear - 1}</option>
         <option value={currYear}>{currYear}</option>
       </select>
-      <select value={meetingKey} onChange={(e) => setMeetingKey(e.target.value)}>
-        {meetingList.map((meetingDetails) => (
-          <option value={meetingDetails.meeting_key} key={meetingDetails.meeting_key}>{meetingDetails.meeting_name}</option>
+      <select value={round} onChange={(e) => setRound(e.target.value)}>
+        {raceList.map((raceDetails) => (
+          <option value={raceDetails.round} key={raceDetails.round}>{raceDetails.raceName}</option>
         ))}
       </select>
-      <select value={sessionKey} onChange={(e) => setSessionKey(e.target.value)}>
-        {sessionList.map((sessionDetails) => (
-          <option value={sessionDetails.session_key} key={sessionDetails.session_key}>{sessionDetails.session_name}</option>
+      <select value={event} onChange={(e) => setEvent(e.target.value)}>
+        {eventList.map((EventDetails) => (
+          <option value={EventDetails.id} key={EventDetails.id}>{EventDetails.name}</option>
         ))}
       </select>
       <span>Results</span>
-      <Table sessionInfo={sessionList.find(s => s.session_key === parseInt(sessionKey))}/>
+      <Table year={year} round={round} event={event}/>
   </>
   )
 }
-
--- src/Table.jsx
+++ src/Table.jsx
@@ -1,57 +1,59 @@
 import { useEffect, useState, useRef } from "react"
 
 // const openF1Endpoint = "https://api.openf1.org/v1/";
-const openF1Endpoint = "http://127.0.0.1:8000/openf1/";
+// const openF1Endpoint = "http://127.0.0.1:8000/openf1/";
 // const jolpicaEndpoint = "http://api.jolpi.ca/";
-// const jolpicaEndpoint = "http://127.0.0.1:8000/jolpica/";
+const jolpicaEndpoint = "http://127.0.0.1:8000/jolpica/";
 
-export default function Table({sessionInfo}) {
-    const sessionKey = sessionInfo?.session_key;
-    const [lapList, setLapList] = useState([]);
-    const [driverList, setDriverList] = useState([]);
-    const [pitList, setPitList] = useState([]);
-    const [raceControlList, setRaceControlList] = useState([]);
-    const [positionList, setPositionList] = useState([]);
+export default function Table({year, round, event}) {
+    // const sessionKey = sessionInfo?.session_key;
+    // const [lapList, setLapList] = useState([]);
+    // const [driverList, setDriverList] = useState([]);
+    // const [pitList, setPitList] = useState([]);
+    // const [raceControlList, setRaceControlList] = useState([]);
+    // const [positionList, setPositionList] = useState([]);
   
-    useEffect(function() {
-        fetch(`${openF1Endpoint}laps/session_key=${sessionKey}`)
-        .then((res) => res.json())
-        .then((data) => {
-            setLapList(data);
-        })
-    }, [sessionKey])
+    // useEffect(function() {
+    //     fetch(`${openF1Endpoint}laps/session_key=${sessionKey}`)
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         setLapList(data);
+    //     })
+    // }, [sessionKey])
   
-    useEffect(function() {
-        fetch(`${openF1Endpoint}drivers/session_key=${sessionKey}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setDriverList(data);
-        })
-    }, [sessionKey])
+    // useEffect(function() {
+    //     fetch(`${openF1Endpoint}drivers/session_key=${sessionKey}`)
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       setDriverList(data);
+    //     })
+    // }, [sessionKey])
   
-    useEffect(function() {
-        fetch(`${openF1Endpoint}pit/session_key=${sessionKey}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setPitList(data);
-        })
-    }, [sessionKey])
+    // useEffect(function() {
+    //     fetch(`${openF1Endpoint}pit/session_key=${sessionKey}`)
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       setPitList(data);
+    //     })
+    // }, [sessionKey])
 
-    useEffect(function() {
-        fetch(`${openF1Endpoint}race_control/session_key=${sessionKey}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setRaceControlList(data);
-        })
-    }, [sessionKey])
+    // useEffect(function() {
+    //     fetch(`${openF1Endpoint}race_control/session_key=${sessionKey}`)
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       setRaceControlList(data);
+    //     })
+    // }, [sessionKey])
 
-    useEffect(function() {
-        fetch(`${openF1Endpoint}position/session_key=${sessionKey}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setPositionList(data);
-        })
-    }, [sessionKey])
+    // useEffect(function() {
+    //     fetch(`${openF1Endpoint}position/session_key=${sessionKey}`)
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       setPositionList(data);
+    //     })
+    // }, [sessionKey])
+
+    
 
     if(sessionInfo?.session_type === "Qualifying") {
         return (
@@ -141,74 +143,74 @@ export default function Table({sessionInfo}) {
     }
   }
 
-  function formatPracticeClassification(lapList, driverList) {
-    const practiceResults =  driverList.map((driver) => {
-        const driverLaps = lapList.filter((lap) => lap.driver_number === driver.driver_number);
-        let fastestLap = 1000;
-        for(var i = 0; i < driverLaps.length; i++) {
-            if(driverLaps[i].lap_duration !== null && driverLaps[i].lap_duration < fastestLap){
-                fastestLap = driverLaps[i].lap_duration
-            }
-        }
-        return {
-            position: 0,
-            driver_name: driver.first_name + " " + driver.last_name,
-            driver_number: driver.driver_number,
-            team_name: driver.team_name,
-            raw_lap_time: fastestLap,
-            lap_time: convertToMMSS(fastestLap),
-            gap: 0,
-            laps: driverLaps.length
-        }
-    })
-    practiceResults.sort(function(a,b){return a.raw_lap_time - b.raw_lap_time})
-    for(var i = 0; i < practiceResults.length; i++) {
-        const allFastestLap = practiceResults[0].raw_lap_time;
-        practiceResults[i].position = i+1;
-        practiceResults[i].gap = practiceResults[i].raw_lap_time === allFastestLap ? "" : "+" + (practiceResults[i].raw_lap_time - allFastestLap).toFixed(3) + "s";
-    }
-    return practiceResults
-}
+//   function formatPracticeClassification(lapList, driverList) {
+//     const practiceResults =  driverList.map((driver) => {
+//         const driverLaps = lapList.filter((lap) => lap.driver_number === driver.driver_number);
+//         let fastestLap = 1000;
+//         for(var i = 0; i < driverLaps.length; i++) {
+//             if(driverLaps[i].lap_duration !== null && driverLaps[i].lap_duration < fastestLap){
+//                 fastestLap = driverLaps[i].lap_duration
+//             }
+//         }
+//         return {
+//             position: 0,
+//             driver_name: driver.first_name + " " + driver.last_name,
+//             driver_number: driver.driver_number,
+//             team_name: driver.team_name,
+//             raw_lap_time: fastestLap,
+//             lap_time: convertToMMSS(fastestLap),
+//             gap: 0,
+//             laps: driverLaps.length
+//         }
+//     })
+//     practiceResults.sort(function(a,b){return a.raw_lap_time - b.raw_lap_time})
+//     for(var i = 0; i < practiceResults.length; i++) {
+//         const allFastestLap = practiceResults[0].raw_lap_time;
+//         practiceResults[i].position = i+1;
+//         practiceResults[i].gap = practiceResults[i].raw_lap_time === allFastestLap ? "" : "+" + (practiceResults[i].raw_lap_time - allFastestLap).toFixed(3) + "s";
+//     }
+//     return practiceResults
+// }
 
-function formatRaceClassification(lapList, driverList, pitList, positionList) {
-    const practiceResults =  driverList.map((driver) => {
-        const driverLaps = lapList.filter((lap) => lap.driver_number === driver.driver_number);
-        let finalPosition = positionList.filter((p) => p.driver_number === driver.driver_number)
-        finalPosition = finalPosition[finalPosition.length - 1]?.position
-        let totalTime = 0;
-        for(var i = 0; i < driverLaps.length; i++) {
-            totalTime += driverLaps[i].lap_duration
-        }
-        return {
-            position: finalPosition,
-            driver_name: driver.first_name + " " + driver.last_name,
-            driver_number: driver.driver_number,
-            team_name: driver.team_name,
-            total_time_s: totalTime,
-            total_time_mmss: convertToMMSS(totalTime),
-            gap: 0,
-            laps: driverLaps.length,
-            pit_stops: pitList.filter((pit) => pit.driver_number === driver.driver_number).length
-        }
-    })
-    practiceResults.sort(function(a,b){return a.position - b.position})
-    for(var i = 0; i < practiceResults.length; i++) {
-        practiceResults[i].gap = 0;
-    }
-    console.log(practiceResults)
-    return practiceResults
-}
+// function formatRaceClassification(lapList, driverList, pitList, positionList) {
+//     const practiceResults =  driverList.map((driver) => {
+//         const driverLaps = lapList.filter((lap) => lap.driver_number === driver.driver_number);
+//         let finalPosition = positionList.filter((p) => p.driver_number === driver.driver_number)
+//         finalPosition = finalPosition[finalPosition.length - 1]?.position
+//         let totalTime = 0;
+//         for(var i = 0; i < driverLaps.length; i++) {
+//             totalTime += driverLaps[i].lap_duration
+//         }
+//         return {
+//             position: finalPosition,
+//             driver_name: driver.first_name + " " + driver.last_name,
+//             driver_number: driver.driver_number,
+//             team_name: driver.team_name,
+//             total_time_s: totalTime,
+//             total_time_mmss: convertToMMSS(totalTime),
+//             gap: 0,
+//             laps: driverLaps.length,
+//             pit_stops: pitList.filter((pit) => pit.driver_number === driver.driver_number).length
+//         }
+//     })
+//     practiceResults.sort(function(a,b){return a.position - b.position})
+//     for(var i = 0; i < practiceResults.length; i++) {
+//         practiceResults[i].gap = 0;
+//     }
+//     console.log(practiceResults)
+//     return practiceResults
+// }
 
-function convertToMMSS(lap) {
-    let result = ""
-    const hours = Math.floor(lap/3600)
-    const minutes = Math.floor((lap - (3600*hours)) / 60);
-    const seconds = (lap - (3600*hours) - (minutes*60)).toFixed(3)
-    if(hours > 0){
-        result += hours + ":"
-        if(minutes < 10){
-            result += "0"
-        }
-    }
-    return (result + minutes + ":" + seconds)
-}
+// function convertToMMSS(lap) {
+//     let result = ""
+//     const hours = Math.floor(lap/3600)
+//     const minutes = Math.floor((lap - (3600*hours)) / 60);
+//     const seconds = (lap - (3600*hours) - (minutes*60)).toFixed(3)
+//     if(hours > 0){
+//         result += hours + ":"
+//         if(minutes < 10){
+//             result += "0"
+//         }
+//     }
+//     return (result + minutes + ":" + seconds)
+// }
